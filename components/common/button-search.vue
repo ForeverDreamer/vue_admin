26d62bfd1556936e18972a78a029f6e4900a1fff
doer <499361328@qq.com>
2020-04-04 19:29:57 +0800

    7.商品列表页开发 4)高级搜索页开发(二) 5)高级搜索页开发(三) 6)数据表格 - 多选 7)数据表格 - 自定义模板

---
+++ components/common/button-search.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="d-flex align-items-cneter">
+      <!--左边-->
+      <slot name="left"></slot>
+      <!--右边-->
+      <slot name="right">
+        <!--多加一层div，否则v-show在d-flex修饰下不生效，应该时v-show切换display属性和d-flex(display: flex)冲突-->
+        <div v-show="!advanceSearch" class="ml-auto">
+          <div class="d-flex align-items-cneter ml-auto">
+            <el-input v-model="keyword" :placeholder="placeholder" size="mini" class="mr-2" />
+            <el-button type="info" size="mini" @click="$emit('search', keyword)">搜索</el-button>
+            <el-button size="mini" @click="advanceSearch = true">高级搜索</el-button>
+          </div>
+        </div>
+      </slot>
+    </div>
+    <el-card v-show="advanceSearch" class="mt-3">
+      <div slot="header" class="clearfix">
+        <span>高级搜索</span>
+        <el-button style="float: right; padding: 3px 0" type="text" @click="closeAdvanceSearch">
+          收起
+        </el-button>
+      </div>
+      <!--表单-->
+      <slot name="searchForm"></slot>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ButtonSearch',
+  props: {
+    placeholder: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      keyword: '',
+      advanceSearch: false
+    }
+  },
+  methods: {
+    closeAdvanceSearch () {
+      this.advanceSearch = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/home/product.vue
+++ pages/home/product.vue
@@ -1,46 +1,124 @@
 <template>
   <div class="px-1">
+    <!--标签选项卡-->
     <el-tabs v-model="activeTabIndex" @tab-click="handleClick">
       <el-tab-pane v-for="(item, index) in tabs" :key="index" :label="item.name">
-        <div class="d-flex align-items-cneter">
-          <el-button type="success" size="mini">
-            发布商品
-          </el-button>
-          <el-button type="success" size="mini">
-            批量删除
-          </el-button>
-          <!--多加一层div，否则v-show在d-flex修饰下不生效，应该时v-show切换display属性和d-flex(display: flex)冲突-->
-          <div v-show="!searchForm.advanceSerch" class="ml-auto">
-            <div class="d-flex align-items-cneter ml-auto">
-              <el-input v-model="searchForm.keyword" placeholder="要搜索的商品名称" size="mini" class="mr-2" />
-              <el-button type="info" size="mini">
-                搜索
-              </el-button>
-              <el-button size="mini" @click="searchForm.advanceSerch = true">
-                高级搜索
-              </el-button>
-            </div>
-          </div>
-        </div>
-        <el-card v-show="searchForm.advanceSerch" class="mt-3">
-          <div slot="header" class="clearfix">
-            <span>高级搜索</span>
-            <el-button style="float: right; padding: 3px 0" type="text" @click="searchForm.advanceSerch = false">
-              收起
-            </el-button>
-          </div>
-          <div>
-            搜索表单
-          </div>
-        </el-card>
+        <button-search ref="buttonSearch" placeholder="要搜索的商品名称" @search="search">
+          <template v-slot:left>
+            <el-button type="success" size="mini">发布商品</el-button>
+            <el-button type="warning" size="mini">恢复商品</el-button>
+            <el-button type="danger" size="mini">批量删除</el-button>
+            <el-button size="mini">上架</el-button>
+            <el-button size="mini">下架</el-button>
+            <el-button size="mini">推荐</el-button>
+          </template>
+          <template v-slot:searchForm>
+            <el-form ref="searchForm" :inline="true" :model="searchForm" class="demo-form-inline">
+              <el-form-item label="商品名称" class="mb-0">
+                <el-input v-model="searchForm.name" size="mini" placeholder="商品名称" />
+              </el-form-item>
+              <el-form-item label="商品编码" class="mb-0">
+                <el-input v-model="searchForm.code" size="mini" placeholder="商品编码" />
+              </el-form-item>
+              <el-form-item label="商品类型" class="mb-0">
+                <el-select v-model="searchForm.type" placeholder="商品类型" size="mini">
+                  <el-option label="类型一" value="type1" />
+                  <el-option label="类型二" value="type2" />
+                </el-select>
+              </el-form-item>
+              <el-form-item label="商品分类" class="mb-0">
+                <el-input v-model="searchForm.category" size="mini" placeholder="商品分类" />
+              </el-form-item>
+              <el-form-item>
+                <el-button type="info" size="mini" @click="search">
+                  搜索
+                </el-button>
+                <el-button size="mini" @click="clearAll">
+                  清空筛选条件
+                </el-button>
+              </el-form-item>
+            </el-form>
+          </template>
+        </button-search>
       </el-tab-pane>
     </el-tabs>
+    <!--商品列表-->
+    <el-table
+      :data="tableData"
+      border
+      class="mt-3"
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column
+        type="selection"
+        width="40"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        label="商品"
+        width="380"
+        align="center">
+        <template slot-scope="scope">
+          <b-media>
+            <template v-slot:aside>
+              <b-img :src="scope.row.coverImage" width="60" alt="placeholder"></b-img>
+            </template>
+
+            <h6 class="mt-0">{{ scope.row.title }}</h6>
+            <p class="mb-1">分类：{{ scope.row.category }}</p>
+            <p class="mb-0">时间：{{ scope.row.createTime }}</p>
+          </b-media>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="type"
+        label="商品类型"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="salesVolume"
+        label="实际销量"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="order"
+        label="商品排序"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        label="商品状态"
+        align="center">
+        <template slot-scope="scope">
+          <el-button :type="scope.row.active ? 'danger' : 'success'" size="mini" plain>{{ scope.row.active ? '下架' : '上架' }}</el-button>
+        </template>
+      </el-table-column>
+      <el-table-column
+        prop="inventory"
+        label="总库存"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="price"
+        label="价格"
+        align="center">
+      </el-table-column>
+      <el-table-column
+        prop="auditStatus"
+        label="操作"
+        align="center">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
+import buttonSearch from '@/components/common/button-search'
+
 export default {
   name: 'Product',
+  components: {
+    buttonSearch
+  },
   data () {
     return {
       tabs: [
@@ -52,14 +130,53 @@ export default {
       ],
       activeTabIndex: 0,
       searchForm: {
-        keyword: '',
-        advanceSerch: false
-      }
+        name: '',
+        code: '',
+        type: '',
+        category: ''
+      },
+      tableData: [{
+        id: 1,
+        title: '荣耀 V10全网通 标配版 4GB+64GB 魅力红',
+        coverImage: 'http://static.yoshop.xany6.com/2018071718294208f086786.jpg',
+        createTime: '2019-07-17 18:34:14',
+        category: '手机',
+        type: '普通商品',
+        salesVolume: 20,
+        order: 100,
+        active: true,
+        inventory: 999,
+        price: 1000,
+        // 0未审核 1通过 2不通过
+        auditStatus: 1
+      }],
+      multipleSelection: []
     }
   },
   methods: {
     handleClick (tab, event) {
       console.log(this.activeTabIndex)
+    },
+    search (param = null) {
+      // 简单搜索，子组件传递字符串参数
+      if (typeof param === 'string') {
+        console.log('简单搜索!', param)
+      } else {
+        // 高级搜索，Vue默认传递$event对象
+        console.log('高级搜索!', param)
+      }
+    },
+    clearAll () {
+      console.log('clearAll!')
+      this.searchForm.name = ''
+      this.searchForm.code = ''
+      this.searchForm.type = ''
+      this.searchForm.category = ''
+      this.$refs.buttonSearch[this.activeTabIndex].closeAdvanceSearch()
+    },
+    handleSelectionChange (val) {
+      console.log(val)
+      this.multipleSelection = val
     }
   }
 }
