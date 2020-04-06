e735602b7bef7e7e9c431de472c96a7dc82481df
doer <499361328@qq.com>
2020-04-06 14:10:12 +0800

    7.商品列表页开发 8)数据表格 - 生成数据和删除功能 9)商品列表 - 分页处理

--- pages/home/product.vue
+++ pages/home/product.vue
@@ -44,7 +44,7 @@
     </el-tabs>
     <!--商品列表-->
     <el-table
-      :data="tableData"
+      :data="tableData[activeTabIndex].products"
       border
       class="mt-3"
       style="width: 100%"
@@ -89,7 +89,15 @@
         label="商品状态"
         align="center">
         <template slot-scope="scope">
-          <el-button :type="scope.row.active ? 'danger' : 'success'" size="mini" plain>{{ scope.row.active ? '下架' : '上架' }}</el-button>
+          <el-button type="success" size="mini" plain @click="scope.row.auditStatus = 1">
+            审核通过
+          </el-button>
+          <el-button type="danger" size="mini" plain class="ml-0 mt-1" @click="scope.row.auditStatus = 0">
+            审核不通过
+          </el-button>
+<!--          <el-button :type="scope.row.active ? 'danger' : 'success'" size="mini" plain @click="changeStatus(scope.row)">-->
+<!--            {{ scope.row.active ? '下架' : '上架' }}-->
+<!--          </el-button>-->
         </template>
       </el-table-column>
       <el-table-column
@@ -99,15 +107,36 @@
       </el-table-column>
       <el-table-column
         prop="price"
-        label="价格"
+        label="价格(元)"
         align="center">
       </el-table-column>
       <el-table-column
         prop="auditStatus"
         label="操作"
+        width="150"
         align="center">
+        <template slot-scope="scope">
+          <el-button-group>
+            <el-button type="primary" size="mini" plain>修改</el-button>
+            <el-button type="danger" size="mini" plain @click="delProduct(scope.$index)">删除</el-button>
+          </el-button-group>
+        </template>
       </el-table-column>
     </el-table>
+    <div style="height: 60px"></div>
+    <el-footer class="border-top d-flex align-items-center px-0 bg-white">
+      <!-- Three values: flex-grow | flex-shrink | flex-basis -->
+      <!-- flex: 2 2 10%; -->
+      <div class="flex-grow-1 px-2">
+        <el-pagination
+          :current-page="tableData[activeTabIndex].currentPage"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+        />
+      </div>
+    </el-footer>
   </div>
 </template>
 
@@ -135,25 +164,39 @@ export default {
         type: '',
         category: ''
       },
-      tableData: [{
-        id: 1,
-        title: '荣耀 V10全网通 标配版 4GB+64GB 魅力红',
-        coverImage: 'http://static.yoshop.xany6.com/2018071718294208f086786.jpg',
-        createTime: '2019-07-17 18:34:14',
-        category: '手机',
-        type: '普通商品',
-        salesVolume: 20,
-        order: 100,
-        active: true,
-        inventory: 999,
-        price: 1000,
-        // 0未审核 1通过 2不通过
-        auditStatus: 1
-      }],
+      tableData: [],
       multipleSelection: []
     }
   },
+  created () {
+    this.initData()
+  },
   methods: {
+    initData () {
+      for (let i = 0; i < this.tabs.length; i++) {
+        this.tableData.push({
+          currentPage: 1,
+          products: []
+        })
+        for (let j = 0; j < 20; j++) {
+          this.tableData[i].products.push({
+            id: j,
+            title: '荣耀 V10全网通 标配版 4GB+64GB 魅力红' + i + '-' + j,
+            coverImage: 'http://static.yoshop.xany6.com/2018071718294208f086786.jpg',
+            createTime: '2019-07-17 18:34:14',
+            category: '手机',
+            type: '普通商品',
+            salesVolume: 20,
+            order: 100,
+            active: true,
+            inventory: 999,
+            price: 1000,
+            // 0未审核 1通过 2不通过
+            auditStatus: 1
+          })
+        }
+      }
+    },
     handleClick (tab, event) {
       console.log(this.activeTabIndex)
     },
@@ -177,11 +220,34 @@ export default {
     handleSelectionChange (val) {
       console.log(val)
       this.multipleSelection = val
+    },
+    delProduct (index) {
+      this.$confirm('此操作将永久删除该文件，是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.tableData[this.activeTabIndex].products.splice(index, 1)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        })
+      }).catch(() => {})
+    },
+    changeStatus (product) {
+      product.active = !product.active
     }
   }
 }
 </script>
 
 <style scoped>
-
+.el-footer {
+  position: fixed;
+  bottom: 0;
+  left: 200px;
+  right: 0;
+  z-index: 1;
+}
+/*.el-backtop的z-index默认是5*/
 </style>
