113d09b81fa85665f60293c17fbfaab814f83cfb
DESKTOP-IS0GHQ8\micro <499361328@qq.com>
2020-08-14 18:16:14 +0800

    例行备份

--- plugins/main.js
+++ plugins/main.js
@@ -4,7 +4,7 @@ import strophe from 'strophe.js'
 import echarts from 'echarts' // 引入echarts
 import VueDND from 'awe-dnd'
 import conf, { doSomething } from '~/utils/common/config'
-import ejabberdInfo from '~/utils/common/ejabberd-info'
+// import ejabberdInfo from '~/utils/common/ejabberd-info'
 // import { dateFilter } from '~/utils/filters/date-filter'
 import '~/utils/filters/register'
 
@@ -15,5 +15,5 @@ Vue.use(VueDND)
 Vue.prototype.$strophe = strophe
 Vue.prototype.$echarts = echarts // 引入组件（将echarts注册为全局）
 Vue.prototype.$conf = conf
-Vue.prototype.$ejabberdInfo = ejabberdInfo
+// Vue.prototype.$ejabberdInfo = ejabberdInfo
 // Vue.filter('formatDate', dateFilter)
